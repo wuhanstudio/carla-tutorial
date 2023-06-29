7863079f2378143012abb62edda692ad8833e3f4
wuhanstudio <wuhanstudio@qq.com>
2023-06-29 13:59:18 +0100

    Fix example 07: Projection error when vehicles are behind the camera

--- 07_3d_bounding_boxes.py
+++ 07_3d_bounding_boxes.py
@@ -11,6 +11,12 @@ import numpy as np
 client = carla.Client('localhost', 2000)
 world  = client.get_world()
 
+# Set up the simulator in synchronous mode
+settings = world.get_settings()
+settings.synchronous_mode = True # Enables synchronous mode
+settings.fixed_delta_seconds = 0.05
+world.apply_settings(settings)
+
 # Get the world spectator
 spectator = world.get_spectator() 
 
@@ -40,34 +46,40 @@ camera.listen(image_queue.put)
 
 # Part 2
 
-def build_projection_matrix(w, h, fov):
+def build_projection_matrix(w, h, fov, is_behind_camera=False):
     focal = w / (2.0 * np.tan(fov * np.pi / 360.0))
     K = np.identity(3)
-    K[0, 0] = K[1, 1] = focal
+
+    if is_behind_camera:
+        K[0, 0] = K[1, 1] = -focal
+    else:
+        K[0, 0] = K[1, 1] = focal
+
     K[0, 2] = w / 2.0
     K[1, 2] = h / 2.0
     return K
 
 def get_image_point(loc, K, w2c):
-        # Calculate 2D projection of 3D coordinate
+    # Calculate 2D projection of 3D coordinate
 
-        # Format the input coordinate (loc is a carla.Position object)
-        point = np.array([loc.x, loc.y, loc.z, 1])
-        # transform to camera coordinates
-        point_camera = np.dot(w2c, point)
+    # Format the input coordinate (loc is a carla.Position object)
+    point = np.array([loc.x, loc.y, loc.z, 1])
+    # transform to camera coordinates
+    point_camera = np.dot(w2c, point)
 
-        # New we must change from UE4's coordinate system to an "standard"
-        # (x, y ,z) -> (y, -z, x)
-        # and we remove the fourth componebonent also
-        point_camera = [point_camera[1], -point_camera[2], point_camera[0]]
+    # New we must change from UE4's coordinate system to an "standard"
+    # (x, y ,z) -> (y, -z, x)
+    # and we remove the fourth componebonent also
+    point_camera = np.array([point_camera[1], -point_camera[2], point_camera[0]]).T
 
-        # now project 3D->2D using the camera matrix
-        point_img = np.dot(K, point_camera)
-        # normalize
-        point_img[0] /= point_img[2]
-        point_img[1] /= point_img[2]
+    # now project 3D->2D using the camera matrix
+    point_img = np.dot(K, point_camera)
 
-        return point_img[0:2]
+    # normalize
+    point_img[0] /= point_img[2]
+    point_img[1] /= point_img[2]
+
+    return point_img
 
 # Remember the edge pairs
 edges = [[0,1], [1,3], [3,2], [2,0], [0,4], [4,5], [5,1], [5,7], [7,6], [6,4], [6,2], [7,3]]
@@ -81,15 +93,26 @@ image_h = camera_bp.get_attribute("image_size_y").as_int()
 fov = camera_bp.get_attribute("fov").as_float()
 
 # Calculate the camera projection matrix to project from 3D -> 2D
-K = build_projection_matrix(image_w, image_h, fov)
+K   = build_projection_matrix(image_w, image_h, fov)
+K_b = build_projection_matrix(image_w, image_h, fov, is_behind_camera=True)
 
 for i in range(20):
-    vehicle_bp = random.choice(bp_lib.filter('vehicle'))
-    npc = world.try_spawn_actor(vehicle_bp, random.choice(spawn_points))
+    vehicle_bp = bp_lib.filter('vehicle')
+
+    # Exclude bicycle
+    car_bp = [bp for bp in vehicle_bp if int(bp.get_attribute('number_of_wheels')) == 4]
+    npc = world.try_spawn_actor(random.choice(car_bp), random.choice(spawn_points))
+
     if npc:
         npc.set_autopilot(True)
 
 edges = [[0,1], [1,3], [3,2], [2,0], [0,4], [4,5], [5,1], [5,7], [7,6], [6,4], [6,2], [7,3]]
+
+def point_in_canvas(pos, img_h, img_w):
+    """Return true if point is in canvas"""
+    if (pos[0] >= 0) and (pos[0] < img_w) and (pos[1] >= 0) and (pos[1] < img_h):
+        return True
+    return False
 
 def clear():
     settings = world.get_settings()
@@ -136,15 +159,32 @@ while True:
                     # of the vehicle and the vector between the vehicle
                     # and the other vehicle. We threshold this dot product
                     # to limit to drawing bounding boxes IN FRONT OF THE CAMERA
-                    forward_vec = vehicle.get_transform().get_forward_vector()
+                    vehicle_forward_vec = vehicle.get_transform().get_forward_vector()
                     ray = npc.get_transform().location - vehicle.get_transform().location
 
-                    if forward_vec.dot(ray) > 1:
-                        p1 = get_image_point(bb.location, K, world_2_camera)
+                    if vehicle_forward_vec.dot(ray) > 1:
                         verts = [v for v in bb.get_world_vertices(npc.get_transform())]
                         for edge in edges:
                             p1 = get_image_point(verts[edge[0]], K, world_2_camera)
                             p2 = get_image_point(verts[edge[1]],  K, world_2_camera)
+
+                            p1_in_canvas = point_in_canvas(p1, image_h, image_w)
+                            p2_in_canvas = point_in_canvas(p2, image_h, image_w)
+
+                            if not p1_in_canvas and not p2_in_canvas:
+                                continue
+
+                            ray0 = verts[edge[0]] - camera.get_transform().location
+                            ray1 = verts[edge[1]] - camera.get_transform().location
+                            cam_forward_vec = camera.get_transform().get_forward_vector()
+
+                            # One of the vertex is behind the camera
+                            if (not (cam_forward_vec.dot(ray0) > 0)) or (not (cam_forward_vec.dot(ray1) > 0)):
+                                if not p1_in_canvas:
+                                    p1 = get_image_point(verts[edge[0]], K_b, world_2_camera)
+                                if not p2_in_canvas:
+                                    p2 = get_image_point(verts[edge[1]], K_b, world_2_camera)
+
                             cv2.line(img, (int(p1[0]),int(p1[1])), (int(p2[0]),int(p2[1])), (255,0,0, 255), 1)        
 
         cv2.imshow('3D Bounding Boxes',img)
