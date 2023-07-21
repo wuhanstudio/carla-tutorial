8185001d78c7daa26be718205b17d079afce49c3
wuhanstudio <wuhanstudio@qq.com>
2023-07-21 09:46:45 +0100

    Example 07: Add 2d bounding boxes

---
+++ 07_2d_bounding_boxes.py
@@ -0,0 +1,229 @@
+import carla
+
+import queue
+import random
+
+import cv2
+import numpy as np
+
+# Part 1
+
+client = carla.Client('localhost', 2000)
+world  = client.get_world()
+
+# Set up the simulator in synchronous mode
+settings = world.get_settings()
+settings.synchronous_mode = True # Enables synchronous mode
+settings.fixed_delta_seconds = 0.05
+world.apply_settings(settings)
+
+# Get the world spectator
+spectator = world.get_spectator() 
+
+# Get the map spawn points
+spawn_points = world.get_map().get_spawn_points()
+
+# spawn vehicle
+bp_lib = world.get_blueprint_library()
+vehicle_bp =bp_lib.find('vehicle.lincoln.mkz_2020')
+vehicle = world.try_spawn_actor(vehicle_bp, random.choice(spawn_points))
+
+# spawn camera
+camera_bp = bp_lib.find('sensor.camera.rgb')
+camera_init_trans = carla.Transform(carla.Location(z=2))
+camera = world.spawn_actor(camera_bp, camera_init_trans, attach_to=vehicle)
+
+# Create a queue to store and retrieve the sensor data
+image_queue = queue.Queue()
+camera.listen(image_queue.put)
+
+# Part 2
+
+def build_projection_matrix(w, h, fov, is_behind_camera=False):
+    focal = w / (2.0 * np.tan(fov * np.pi / 360.0))
+    K = np.identity(3)
+
+    if is_behind_camera:
+        K[0, 0] = K[1, 1] = -focal
+    else:
+        K[0, 0] = K[1, 1] = focal
+
+    K[0, 2] = w / 2.0
+    K[1, 2] = h / 2.0
+    return K
+
+def get_image_point(loc, K, w2c):
+    # Calculate 2D projection of 3D coordinate
+
+    # Format the input coordinate (loc is a carla.Position object)
+    point = np.array([loc.x, loc.y, loc.z, 1])
+    # transform to camera coordinates
+    point_camera = np.dot(w2c, point)
+
+    # New we must change from UE4's coordinate system to an "standard"
+    # (x, y ,z) -> (y, -z, x)
+    # and we remove the fourth componebonent also
+    point_camera = np.array([point_camera[1], -point_camera[2], point_camera[0]]).T
+
+    # now project 3D->2D using the camera matrix
+    point_img = np.dot(K, point_camera)
+
+    # normalize
+    point_img[0] /= point_img[2]
+    point_img[1] /= point_img[2]
+
+    return point_img
+
+# Remember the edge pairs
+edges = [[0,1], [1,3], [3,2], [2,0], [0,4], [4,5], [5,1], [5,7], [7,6], [6,4], [6,2], [7,3]]
+
+# Get the world to camera matrix
+world_2_camera = np.array(camera.get_transform().get_inverse_matrix())
+
+# Get the attributes from the camera
+image_w = camera_bp.get_attribute("image_size_x").as_int()
+image_h = camera_bp.get_attribute("image_size_y").as_int()
+fov = camera_bp.get_attribute("fov").as_float()
+
+# Calculate the camera projection matrix to project from 3D -> 2D
+K   = build_projection_matrix(image_w, image_h, fov)
+K_b = build_projection_matrix(image_w, image_h, fov, is_behind_camera=True)
+
+for i in range(20):
+    vehicle_bp = bp_lib.filter('vehicle')
+
+    # Exclude bicycle
+    car_bp = [bp for bp in vehicle_bp if int(bp.get_attribute('number_of_wheels')) == 4]
+    npc = world.try_spawn_actor(random.choice(car_bp), random.choice(spawn_points))
+
+    if npc:
+        npc.set_autopilot(True)
+
+# Retrieve all the objects of the level
+car_objects = world.get_environment_objects(carla.CityObjectLabel.Car) # doesn't have filter by type yet
+truck_objects = world.get_environment_objects(carla.CityObjectLabel.Truck) # doesn't have filter by type yet
+bus_objects = world.get_environment_objects(carla.CityObjectLabel.Bus) # doesn't have filter by type yet
+
+env_object_ids = []
+
+for obj in (car_objects + truck_objects + bus_objects):
+    env_object_ids.append(obj.id)
+
+# Disable all static vehicles
+world.enable_environment_objects(env_object_ids, False) 
+
+edges = [[0,1], [1,3], [3,2], [2,0], [0,4], [4,5], [5,1], [5,7], [7,6], [6,4], [6,2], [7,3]]
+
+def point_in_canvas(pos, img_h, img_w):
+    """Return true if point is in canvas"""
+    if (pos[0] >= 0) and (pos[0] < img_w) and (pos[1] >= 0) and (pos[1] < img_h):
+        return True
+    return False
+
+def clear():
+    settings = world.get_settings()
+    settings.synchronous_mode = False # Disables synchronous mode
+    settings.fixed_delta_seconds = None
+    world.apply_settings(settings)
+
+    camera.stop()
+
+    for npc in world.get_actors().filter('*vehicle*'):
+        if npc:
+            npc.destroy()
+
+    print("Vehicles Destroyed.")
+
+# Main Loop
+vehicle.set_autopilot(True)
+
+while True:
+    try:
+        world.tick()
+
+        # Move the spectator to the top of the vehicle 
+        transform = carla.Transform(vehicle.get_transform().transform(carla.Location(x=-4,z=50)), carla.Rotation(yaw=-180, pitch=-90)) 
+        spectator.set_transform(transform) 
+
+        # Retrieve and reshape the image
+        image = image_queue.get()
+
+        img = np.reshape(np.copy(image.raw_data), (image.height, image.width, 4))
+
+        # Get the camera matrix 
+        world_2_camera = np.array(camera.get_transform().get_inverse_matrix())
+
+        for npc in world.get_actors().filter('*vehicle*'):
+
+            # Filter out the ego vehicle
+            if npc.id != vehicle.id:
+
+                bb = npc.bounding_box
+                dist = npc.get_transform().location.distance(vehicle.get_transform().location)
+
+                # Filter for the vehicles within 50m
+                if dist < 50:
+
+                # Calculate the dot product between the forward vector
+                # of the vehicle and the vector between the vehicle
+                # and the other vehicle. We threshold this dot product
+                # to limit to drawing bounding boxes IN FRONT OF THE CAMERA
+                    forward_vec = vehicle.get_transform().get_forward_vector()
+                    ray = npc.get_transform().location - vehicle.get_transform().location
+
+                    if forward_vec.dot(ray) > 0:
+                        verts = [v for v in bb.get_world_vertices(npc.get_transform())]
+                        x_max = -10000
+                        x_min = 10000
+                        y_max = -10000
+                        y_min = 10000
+
+                        for vert in verts:
+                            p = get_image_point(vert, K, world_2_camera)
+                            p_in_canvas = point_in_canvas(p, image_h, image_w)
+
+                            ray0 = vert - camera.get_transform().location
+                            cam_forward_vec = camera.get_transform().get_forward_vector()
+
+                            if not (cam_forward_vec.dot(ray0) > 0):
+                                p = get_image_point(vert, K_b, world_2_camera)
+
+                            if p[0] < 0:
+                                p[0] = 0
+                            if p[0] > image.width:
+                                p[0] = image.width
+                            
+                            if p[1] < 0:
+                                p[1] = 0
+                            if p[1] > image.height:
+                                p[1] = image.height
+
+                            # Find the rightmost vertex
+                            if p[0] > x_max:
+                                x_max = p[0]
+                            # Find the leftmost vertex
+                            if p[0] < x_min:
+                                x_min = p[0]
+                            # Find the highest vertex
+                            if p[1] > y_max:
+                                y_max = p[1]
+                            # Find the lowest  vertex
+                            if p[1] < y_min:
+                                y_min = p[1]
+
+                        cv2.line(img, (int(x_min),int(y_min)), (int(x_max),int(y_min)), (0,0,255, 255), 1)
+                        cv2.line(img, (int(x_min),int(y_max)), (int(x_max),int(y_max)), (0,0,255, 255), 1)
+                        cv2.line(img, (int(x_min),int(y_min)), (int(x_min),int(y_max)), (0,0,255, 255), 1)
+                        cv2.line(img, (int(x_max),int(y_min)), (int(x_max),int(y_max)), (0,0,255, 255), 1)
+
+        cv2.imshow('3D Bounding Boxes',img)
+
+        if cv2.waitKey(1) == ord('q'):
+            clear()
+            break
+
+    except KeyboardInterrupt as e:
+        clear()
+        break
+
+cv2.destroyAllWindows()
